7fce3aa9466d5b295e73c3443442da3a92ba5dfa
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-05-01 18:51:19 +0200

    Code cleanup, lazy schemes loading

--- lib/iuliia/iuliia.rb
+++ lib/iuliia/iuliia.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Iuliia
-  module_function
-
-  def translit(string, schema:)
-    Iuliia::Translit.new(string, schema).translit
+  class << self
+    def translit(string, schema:)
+      Iuliia::Translit.new(string, schema).translit
+    end
   end
 end
