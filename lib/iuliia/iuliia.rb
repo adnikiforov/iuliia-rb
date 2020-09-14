94afa2ce93c252480f7ec2592f50adff92c3c47d
Andrey Nikiforov <a.d.nikiforov@gmail.com>
2020-09-14 14:28:11 +0400

    Add deprecation warnings

--- lib/iuliia/iuliia.rb
+++ lib/iuliia/iuliia.rb
@@ -2,12 +2,13 @@
 
 module Iuliia
   class << self
-
     # Translit cyrillic string to latin representation
     # @param string [String]
     # @param schema [Iuliia::Schema]
     # @return [String]
     def translit(string, schema:)
+      warn "[DEPRECATION] Gem has been renamed to 'iuliia'. Please, use 'iuliia' instead."
+
       Iuliia::Translit.new(string, schema).translit
     end
   end
